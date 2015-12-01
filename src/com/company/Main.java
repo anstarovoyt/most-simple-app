355c9d129b3c4b23fcdbc49f590748f61c2821dc
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2015-12-02 01:05:25 +0300

    add resources

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import org.json.JSONObject;
 public class Main {
 
     public static void main(String[] args) {
-        JSONObject jsonObject = new JSONObject("{p:3}");
+        JSONObject jsonObject = new JSONObject("{p:4}");
 
         System.out.println(jsonObject.get("p"));
     }
