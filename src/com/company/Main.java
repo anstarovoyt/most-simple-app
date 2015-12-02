96c605a0d0629380e243db771945c6b93c6f5e9b
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2015-12-02 20:12:10 +0300

    Add parsing external file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,16 @@
 package com.company;
 
 import org.json.JSONObject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
-    public static void main(String[] args) {
-        JSONObject jsonObject = new JSONObject("{p:4}");
+    public static void main(String[] args) throws Exception {
+        String content = new String(Files.readAllBytes(Paths.get("way.json")));
+        JSONObject jsonObject = new JSONObject(content);
 
-        System.out.println(jsonObject.get("p"));
+        System.out.println("Content2:" + jsonObject.get("points"));
     }
 }
